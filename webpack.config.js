54890edbebf286101629416b6ff8c247690c812f
kkindorf <kevin.kindorf@gmail.com>
2018-02-18 14:26:36 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path")
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'blahr',
+            minify: {
+                collapseWhitespace: true
+            },
+
+            template: './src/index.html'
+        })]
+}
